62693c4a0591148c4c7a7b1b0b32beaec0549c55
adelmarcio0 <adelmarcio0@gmail.com>
2023-05-20 14:30:00 +0800

    feat: setup smart contracts development environment
    - Add Hardhat configuration for multi-network deployment
    - Configure Solidity compiler with optimization settings
    - Setup package.json with deployment and testing scripts
    - Add support for Polygon and Arbitrum networks
    - Include gas reporting and contract verification tools

---
+++ contracts/hardhat.config.js
@@ -0,0 +1,40 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: {
+    version: "0.8.19",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    localhost: {
+      url: "http://127.0.0.1:8545"
+    },
+    polygon: {
+      url: process.env.POLYGON_RPC_URL || "https://polygon-rpc.com",
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : []
+    },
+    arbitrum: {
+      url: process.env.ARBITRUM_RPC_URL || "https://arb1.arbitrum.io/rpc",
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : []
+    }
+  },
+  etherscan: {
+    apiKey: {
+      polygon: process.env.POLYGONSCAN_API_KEY,
+      arbitrumOne: process.env.ARBISCAN_API_KEY
+    }
+  },
+  gasReporter: {
+    enabled: process.env.REPORT_GAS !== undefined,
+    currency: "USD"
+  }
+};
