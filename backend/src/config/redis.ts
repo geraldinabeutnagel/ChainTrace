d32e028ef5bb165df5b06e2ba3524067cd8260a7
adelmarcio0 <adelmarcio0@gmail.com>
2023-10-20 11:30:00 +0800

    feat: implement Redis configuration and connection management
    - Add Redis client creation with configurable connection options
    - Implement connection retry strategy with exponential backoff
    - Add comprehensive error handling and event listeners
    - Include health check functionality for Redis connection
    - Add connection cleanup and graceful shutdown support

---
+++ backend/src/config/redis.ts
@@ -0,0 +1,80 @@
+import { createClient, RedisClientType } from 'redis';
+import { logger } from '../utils/logger';
+
+let redisClient: RedisClientType | null = null;
+
+export const connectRedis = async (): Promise<void> => {
+  try {
+    redisClient = createClient({
+      host: process.env.REDIS_HOST || 'localhost',
+      port: parseInt(process.env.REDIS_PORT || '6379', 10),
+      password: process.env.REDIS_PASSWORD,
+      db: parseInt(process.env.REDIS_DB || '0', 10),
+      retry_strategy: (options) => {
+        if (options.error && options.error.code === 'ECONNREFUSED') {
+          logger.error('Redis server connection refused');
+          return new Error('Redis server connection refused');
+        }
+        if (options.total_retry_time > 1000 * 60 * 60) {
+          logger.error('Redis retry time exhausted');
+          return new Error('Retry time exhausted');
+        }
+        if (options.attempt > 10) {
+          logger.error('Redis max retry attempts reached');
+          return undefined;
+        }
+        return Math.min(options.attempt * 100, 3000);
+      }
+    });
+
+    redisClient.on('error', (error) => {
+      logger.error('Redis client error:', error);
+    });
+
+    redisClient.on('connect', () => {
+      logger.info('Redis client connected');
+    });
+
+    redisClient.on('ready', () => {
+      logger.info('Redis client ready');
+    });
+
+    redisClient.on('end', () => {
+      logger.info('Redis client connection ended');
+    });
+
+    await redisClient.connect();
+    logger.info('Redis connection established successfully');
+  } catch (error) {
+    logger.error('Redis connection failed:', error);
+    throw error;
+  }
+};
+
+export const getRedisClient = (): RedisClientType => {
+  if (!redisClient) {
+    throw new Error('Redis client not initialized');
+  }
+  return redisClient;
+};
+
+export const closeRedis = async (): Promise<void> => {
+  if (redisClient) {
+    await redisClient.quit();
+    redisClient = null;
+    logger.info('Redis connection closed');
+  }
+};
+
+export const checkRedisHealth = async (): Promise<boolean> => {
+  try {
+    if (!redisClient) {
+      return false;
+    }
+    await redisClient.ping();
+    return true;
+  } catch (error) {
+    logger.error('Redis health check failed:', error);
+    return false;
+  }
+};
